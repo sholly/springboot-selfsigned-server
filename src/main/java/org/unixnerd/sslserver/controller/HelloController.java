98c7fe372f78ec02549314cfbf9d25994f36b7e7
Jay Shollenberger <jshollen@redhat.com>
2021-05-11 17:28:35 -0600

    endpoint log

--- src/main/java/org/unixnerd/sslserver/controller/HelloController.java
+++ src/main/java/org/unixnerd/sslserver/controller/HelloController.java
@@ -1,5 +1,7 @@
 package org.unixnerd.sslserver.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,11 @@ import java.util.Date;
 @RestController
 public class HelloController {
 
+    private Logger log = LoggerFactory.getLogger(HelloController.class);
     @GetMapping("/hello")
     public String hello() {
         Date d = new Date();
+        log.info("endpoint called at: " + d.toString());
         return "hello from server, time is: " + d.toString();
     }
 
